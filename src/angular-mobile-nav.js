b262bc365e0994e9c73e17433383447be26de496
Dominic English <dominic.b.english@gmail.com>
2014-02-27 16:24:54 +1000

    improve(factory): adds a class toggle on content div

--- src/angular-mobile-nav.js
+++ src/angular-mobile-nav.js
@@ -8,16 +8,22 @@ angular.module('de.angular-mobile-nav', [])
         navigation.toggle = function toggle() {
             var navigation = angular.element(document.querySelector('.de-nav'));
             navigation.toggleClass('show');
+            var content = angular.element(document.querySelector('.de-content'));
+            content.toggleClass('nav-open');
         };
 
         navigation.show = function show() {
             var navigation = angular.element(document.querySelector('.de-nav'));
             navigation.addClass('show');
+            var content = angular.element(document.querySelector('.de-content'));
+            content.addClass('nav-open');
         };
 
         navigation.hide = function hide() {
             var navigation = angular.element(document.querySelector('.de-nav'));
             navigation.removeClass('show');
+            var content = angular.element(document.querySelector('.de-content'));
+            content.removeClass('nav-open');
         };
 
         return navigation;
